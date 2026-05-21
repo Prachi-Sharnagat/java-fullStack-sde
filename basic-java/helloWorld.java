5170282ee5d2b3038c57629516b7ed49225e2677
Prachi Sharnagat <prachisharnagat06@gmail.com>
2026-05-21 23:41:48 +0530

    updated code

---
+++ basic-java/helloWorld.java
@@ -0,0 +1,16 @@
+class helloWorld {
+    
+    public static void main(String a[]){
+        float marks = 9.3;
+        byte mar = 9.2;
+
+        int num1 = 10;
+        int num2 = 90;
+        System.out.println(num1+num2);
+        System.out.println(marks);
+
+        System.out.println(9+10);
+        System.out.println("hello world");
+    }
+
+}
